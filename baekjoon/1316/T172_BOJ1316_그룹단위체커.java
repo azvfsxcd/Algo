b0af2b07ebbd7f4349bd873c8f4687f3e1bc7764
김재민 <sxbgdcvf@naver.com>
2021-07-25 20:02:18 +0900

    Add code BOJ #1316

---
+++ baekjoon/1316/T172_BOJ1316_그룹단위체커.java
@@ -0,0 +1,45 @@
+package test07;
+
+import java.util.Scanner;
+
+public class T172_BOJ1316_그룹단위체커 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int cnt = 0;
+		int num = sc.nextInt();
+		
+
+		for (int i = 0; i < num; i++) {
+			boolean test=true;
+			boolean[] check = new boolean[26];
+			int prev = 0;
+			String str = sc.next();
+			
+			for (int j = 0; j < str.length(); j++) {
+				int now = str.charAt(j);
+
+				if (prev != now) {
+					if (check[now - 'a'] == false) {
+						check[now - 'a'] = true;
+						prev = now;
+					} else {
+						test = false;
+						break;
+					}
+				} else {
+					continue;
+				}
+			}
+			
+			if(test==true) {
+				cnt++;
+			}
+			
+		}
+
+		System.out.println(cnt);
+	}
+}
+
+	
